c05628939778a741794098ef77790580a3baddc8
Victoria Deguara <05victoriadeguara@gmial.com>
2026-07-07 13:15:19 +0200

    Adding text_2 file to repository

---
+++ text_2.py
@@ -0,0 +1,9 @@
+myCat = {'size': 'fat', 'colour': 'grey', 'disposition': 'loud'}
+myCat['size']
+print('my cat has ' + myCat['colour'] + ' fur')
+
+books = {'colour': 'gray', 'disposition': 'loud', 'size': 'fat'}
+if books ==myCat:
+    print('books and myCat are the same')
+else: print ('that is not right')
+
