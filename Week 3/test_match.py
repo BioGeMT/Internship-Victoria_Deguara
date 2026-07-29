46ea9ea4e95fa87fc448b05cc94b6610742c034a
Victoria Deguara <05victoriadeguara@gmial.com>
2026-07-29 17:36:13 +0200

    Seed model match on all test sets

---
+++ Week 3/test_match.py
@@ -0,0 +1,50 @@
+import pandas as pd 
+import argparse
+
+#defining the seed-type functions (from first task)
+
+def get_seed6mer(noncodingRNA):
+    return [noncodingRNA[1:7],noncodingRNA[2:8],'A'+noncodingRNA[1:6]]
+
+def get_seed7mer(noncodingRNA) :
+    return [noncodingRNA[1:8],'A'+noncodingRNA[1:7]]
+
+def get_seed8mer(noncodingRNA):
+    return ['A'+noncodingRNA[1:8]]
+
+#definig the match-checking function
+
+def check_match(row, seed_function):
+    seeds = seed_function(row["noncodingRNA"])
+    for seed in seeds:
+        if seed in row["gene"]:
+            return 1
+    return 0
+
+def check_match_6mer(row):
+    return check_match(row, get_seed6mer)
+
+def check_match_7mer(row):
+    return check_match(row, get_seed7mer)
+
+def check_match_8mer(row):
+    return check_match(row, get_seed8mer)
+
+#setting up argparse
+
+def seed_match_df():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file")
+    parser.add_argument("output_file")
+    args = parser.parse_args()
+
+    dfi = pd.read_csv(args.input_file, sep='\t')[["gene", "noncodingRNA", "label"]]
+
+    dfi["seed6mer"] = dfi.apply(check_match_6mer, axis=1)
+    dfi["seed7mer"] = dfi.apply(check_match_7mer, axis=1)
+    dfi["seed8mer"] = dfi.apply(check_match_8mer, axis=1)
+
+    dfi.to_csv(args.output_file)
+
+if __name__ == "__main__":
+    seed_match_df()
